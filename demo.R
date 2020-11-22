12a5a738abdd3f573f5934890368cd6cef61fa27
SantiagoRugelesSch <ansarusc@gmail.com>
2020-11-22 02:05:39 -0500

    Adding R main function

--- demo.R
+++ demo.R
@@ -2,7 +2,7 @@
 # date: 2020-11-22
 
 "This script prints out docopt args.
-Usage: demo.R <arg1> --arg2=<arg2> [--arg3=<arg3>] [--arg4=<arg4>]
+Usage: demo.R <arg> --arg2=<arg2> [--arg3=<arg3>] [--arg4=<arg4>]
 
 Options:
 <arg>             Takes any value (this is a required positional argument)
@@ -13,6 +13,15 @@ Options:
 
 library(docopt)
 opt <- docopt(doc)
-print(opt$arg4)
-print(opt)
-print(typeof(opt))
+main <- function(arg, arg2, arg3=NULL, arg4=NULL) {
+  vector <- c(arg1, arg2, arg3, arg4)
+  print(arg4)
+  for (i in vector) {
+    print(i)
+    print(typeof(i))
+  }
+  
+}
+  
+
+main(opt$arg, opt$arg2, opt$arg3, opt$arg4)
